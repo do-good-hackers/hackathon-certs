d9f5210b1b58d1cf6542648db25dc7ab840891dd
aquadesk <aquadesk.cs@gmail.com>
2021-10-02 23:08:29 -0700

    add filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import './App.css';
 import { useParams } from 'react-router-dom';
 import cert1Bg from './cert1-bg.jpg';
+
+import data from './data';
 
 interface Params {
   participant: string;
@@ -11,17 +12,27 @@ function App() {
   const params = useParams<Params>();
   const { participant } = params;
 
+  const notFound = !data.participants.includes(participant);
+
   return (
     <div className="relative h-full w-xl">
-      <div className="top-0 absolute">
-        <img src={cert1Bg} alt="do good hackers certs" />
-      </div>
-      <div
-        className="absolute font-bold"
-        style={{ left: '21vw', top: '35vw', fontSize: '3vw' }}
-      >
-        {participant}
-      </div>
+      {notFound ? (
+        <div className="text-center p-32">
+          {participant} 참여자 찾을 수 없음 (관리자에게 문의해주세요)
+        </div>
+      ) : (
+        <>
+          <div className="top-0 absolute">
+            <img src={cert1Bg} alt="do good hackers certs" />
+          </div>
+          <div
+            className="absolute font-bold"
+            style={{ left: '21vw', top: '35vw', fontSize: '3vw' }}
+          >
+            {participant}
+          </div>
+        </>
+      )}
     </div>
   );
 }
