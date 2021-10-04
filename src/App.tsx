3f5b151dc84a14a9b7d0d0ff1cee09b28e661a5b
aquadesk <aquadesk.cs@gmail.com>
2021-10-03 23:37:56 -0700

    add download button

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,67 @@
+import { useRef } from 'react';
 import './App.css';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { exportComponentAsJPEG } from 'react-component-export-image';
+
 import cert1Bg from './cert1-bg.jpg';
 
 import data from './data';
 
-interface Params {
-  participant: string;
+export function useQueryParams<K, T>(keys: K[]): T {
+  const params = new URLSearchParams(useLocation().search);
+
+  return keys.reduce<T>((p, key: K) => {
+    // @ts-ignore
+    p[key] = params.get(key);
+    return p;
+  }, {} as T);
 }
 
 function App() {
-  const params = useParams<Params>();
-  const { participant } = params;
+  const params = useQueryParams<string, Record<string, string>>([
+    'p',
+  ]);
+  const participant = params.p;
 
   const notFound = !data.participants.includes(participant);
 
+  const componentRef = useRef(null);
+
   return (
-    <div className="relative h-full w-xl">
-      {notFound ? (
-        <div className="text-center p-32">
-          {participant} 참여자 찾을 수 없음 (관리자에게 문의해주세요)
-        </div>
-      ) : (
-        <>
-          <div className="top-0 absolute">
-            <img src={cert1Bg} alt="do good hackers certs" />
+    <div className="relative min-h-full w-xl">
+      <button
+        className="p-5 m-5 bg-blue-300 text-gray-800 font-extrabold rounded"
+        onClick={() => exportComponentAsJPEG(componentRef)}
+      >
+        다운로드
+      </button>
+
+      <div className="relative w-xl border-2">
+        {notFound ? (
+          <div className="text-center p-32">
+            {participant} 참여자 찾을 수 없음 (관리자에게
+            문의해주세요)
           </div>
-          <div
-            className="absolute font-bold"
-            style={{ left: '21vw', top: '35vw', fontSize: '3vw' }}
-          >
-            {participant}
+        ) : (
+          <div className="">
+            <div className="top-0 absolute border-gray-200">
+              <div ref={componentRef}>
+                <img src={cert1Bg} alt="do good hackers certs" />
+                <div
+                  className="absolute font-bold"
+                  style={{
+                    left: '21vw',
+                    top: '35vw',
+                    fontSize: '3vw',
+                  }}
+                >
+                  {participant}
+                </div>
+              </div>
+            </div>
           </div>
-        </>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import "./index.css";
-import "./tailwind.css";
+import './index.css';
+import './tailwind.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/:participant" component={App} />
+        <Route exact path="/hackathon-certs" component={App} />
         <Route path="/">404</Route>
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
